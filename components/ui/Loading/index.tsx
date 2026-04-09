1c23d07b8ceb6a9ba434785c96a7dc0533e88d09
Andrei Kalpovski <kalpovskii@gmail.com>
2026-04-09 18:49:49 +0300

    move: edit project structure

--- components/ui/Loading/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@/components/ui/spinner";
-
-export default function Loading() {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/70">
-      <Spinner className="text-primary h-12 w-12" />
-    </div>
-  );
-}
